3924a1f5e20ccc20c7b6de14b653af82f8600422
Sergiohc19 <161878354+Sergiohc19@users.noreply.github.com>
2024-06-14 23:40:38 +0000

    feat: add fetch calls for Star Wars API data
    Added fetch calls to retrieve arrays of characters, vehicles, and planets from the Star Wars API, We used useEffect to fetch the contents of these arrays and ensure they are fetched when the main page reloads

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,30 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			characters: [],
+			vehicles: [],
+			planets: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			getCharacters: async () => {
+				const res = await fetch("https://www.swapi.tech/api/people/")
+				const data = await res.json()
+				console.log(data)
+				setStore({ characters: data.results })
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+
+			getVehicles: async () => {
+				await fetch("https://www.swapi.tech/api/vehicles/")
+				const data = await res.json()
+				console.log(data)
+				setStore({ vehicles: data.results })
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			getPlanets: async () => {
+				await fetch("https://www.swapi.tech/api/planets/")
+				const data = await res.json()
+				console.log(data)
+				setStore({ planets: data.results })
 			}
 		}
 	};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,45 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+import "../../styles/home.css";
+import React, { useState, useEffect, useContext } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+	useEffect(() => {
+		actions.getCharacters()
+		actions.getVehicles()
+		actions.getPlanets()
+	}, []);
+
+	return (
+		<div className="text-center mt-5">
+
+			<h1>Star Wars!</h1>
+			
+			<div className="d-flex">
+				
+			
+			<div className="characters">
+				<h2>Characters</h2>
+			</div>
+
+
+			<div className="vehicles">
+				<h2>Vehicles</h2>
+			</div>
+
+
+
+			<div className="planets"> 
+				<h2>Planets</h2>
+			</div>
+
+			</div>
+
+
+		</div>
+
+
+	);
+};
